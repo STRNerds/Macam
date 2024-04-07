04c9d725877c8ab777183bd1800dd31497d1ce61
Ethan Yan Xu <ethanxucoder@gmail.com>
2024-04-07 13:53:48 -0400

    Initial Commit

---
+++ Macam/MacamApp.swift
@@ -0,0 +1,17 @@
+//
+//  MacamApp.swift
+//  Macam
+//
+//  Created by Ethan Xu on 2024-04-07.
+//
+
+import SwiftUI
+
+@main
+struct MacamApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
