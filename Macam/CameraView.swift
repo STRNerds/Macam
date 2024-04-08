528dd9d41882ed2987785361c32193ffadd350a2
Ethan Yan Xu <ethanxucoder@gmail.com>
2024-04-08 09:13:09 -0400

    Initial Commit

---
+++ Macam/CameraView.swift
@@ -0,0 +1,18 @@
+//
+//  CameraView.swift
+//  Macam
+//
+//  Created by Ethan Xu on 2024-04-07.
+//
+
+import SwiftUI
+
+struct CameraView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CameraView()
+}
